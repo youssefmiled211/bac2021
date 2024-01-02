090c158ef6488a731c3e1dad3a125b2cc812dda5
youssefmiled211 <150190494+youssefmiled211@users.noreply.github.com>
2024-01-02 19:11:09 +0100

    Add files via upload

---
+++ fonc.js
@@ -0,0 +1,16 @@
+function verif()
+{
+    sat=document.getElementById("sta").selectedIndex;
+    sai=document.getElementById("sai").selectedIndex;
+    2018=document.getElementById("2018").checked;
+    2019=document.getElementById("2019").checked;
+    temp=document.getElementById("temp").value;
+    pel=document.getElementById("pel").value;
+    alert("erttyui")
+    if(sat==0){
+        alert("chouir une statistique");
+    }
+    if(sai==0){
+        alert("choisir une saison")
+    }
+}
